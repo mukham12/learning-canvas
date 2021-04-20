7735ec71cecd4936d539dcf0cc247a81b2e65425
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-04-20 12:07:41 -0400

    Basic Text Display

---
+++ Canvas Text API/app.js
@@ -0,0 +1,6 @@
+let canvas = document.getElementById('my-canvas')
+let context = canvas.getContext('2d')
+
+context.font = '50px serif'
+context.fillStyle = '#FF0000'
+context.fillText('Hello World', 100, 80)
