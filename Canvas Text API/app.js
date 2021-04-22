4f3cc288dd840674c55598c4a09ef58b1ee33640
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-04-22 00:03:36 -0400

    Measuring text

--- Canvas Text API/app.js
+++ Canvas Text API/app.js
@@ -2,17 +2,19 @@ let canvas = document.getElementById('my-canvas')
 let context = canvas.getContext('2d')
 
 let message = 'your text'
-const drawScreen = () => {
-  context.font = '50px serif'
-  context.fillText(message, 100, 80)
-  context.fillStyle = '#FF0000'
-}
+// const drawScreen = () => {
+//   context.font = '50px serif'
+//   context.fillText(message, 100, 80)
+//   context.fillStyle = '#FF0000'
+// }
 
-let formElement = document.getElementById('textBox')
-formElement.addEventListener('keyup', textBoxChanged, false)
+// let formElement = document.getElementById('textBox')
+// formElement.addEventListener('keyup', textBoxChanged, false)
 
-const textBoxChanged = (e) => {
-  let target = e.target
-  message = target.value
-  drawScreen()
-}
+// const textBoxChanged = (e) => {
+//   let target = e.target
+//   message = target.value
+//   drawScreen()
+// }
+
+let metrics = context.measureText(message)
