e882a8790d5880ee781bdb11c78c1e5f956b76c3
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-05-14 16:18:49 -0400

    HTML and JS template

--- Canvas Images/app.js
+++ Canvas Images/app.js
@@ -1,4 +1,13 @@
 let canvas = document.getElementById('my-canvas')
 let context = canvas.getContext('2d')
 
+const drawScreen = () => {
+  context.fillStyle = '#aaaaaa'
+  context.fillRect(0, 0, 200, 200)
+  context.fillStyle = '#000000'
+  context.font = '20px sans-serif'
+  context.textBaseline = 'top'
+  context.fillText('Canvas!', 0, 0)
+}
 
+drawScreen()
